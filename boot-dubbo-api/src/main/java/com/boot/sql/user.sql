6933f54fd73a59245c9d682743803ac5ec393dda
jason_YJS <596185079@qq.com>
2018-08-16 16:08:43 +0800

    spring boot  mybatis  dubbo thymleaf 整合第一次 提交

---
+++ boot-dubbo-api/src/main/java/com/boot/sql/user.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` (`id`, `age`, `name`) VALUES (1, 20, 'aaa');
+INSERT INTO `user` (`id`, `age`, `name`) VALUES (2, 30, 'bbb');
+INSERT INTO `user` (`id`, `age`, `name`) VALUES (8, 10, 'AAA');
